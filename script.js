4bf9531fef304ddbd33708a217a725557fa42973
sonalira <68660685+sonalira@users.noreply.github.com>
2020-08-25 00:31:37 -0700

    Batch update

---
+++ script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":960},"pages":[{"id":"design","displayName":"Design","link":{"type":"LinkTypePage","value":"#!page-design"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"photo","displayName":"Photo","link":{"type":"LinkTypePage","value":"#!page-photo"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"code","displayName":"Code","link":{"type":"LinkTypePage","value":"#!page-code"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
